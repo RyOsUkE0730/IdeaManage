06c9f6369edb386c0389e05ae4e6d2f278602bdf
RyOsUkE <ryosukek74@gmail.com>
2022-03-25 16:45:51 +0900

    Update controller

---
+++ app/controllers/ideas_controller.rb
@@ -0,0 +1,25 @@
+class IdeasController < ApplicationController
+  def index
+    @category = inputs[:category]
+    if @category.nil?
+      @idea = Idea.all
+    else
+      @idea = Idea.find(@category)
+    end
+  end
+
+  def new
+    @idea = Idea.new
+  end
+
+  def create
+    @idea = Idea.new(idea_params)
+    if @item.valid?
+      @item.save
+      render status: 201, json: { status: 201, message: "Success" }
+    else
+      render status: 422, json: { status: 422, message: "Error"}
+    end
+  end
+
+end
