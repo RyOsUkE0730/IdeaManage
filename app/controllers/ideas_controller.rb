33c6b2efce1bf25eb744703d7a569ee54b5a75c2
RyOsUkE <ryosukek74@gmail.com>
2022-03-27 20:31:08 +0900

    Update ideas_controller.rb

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,30 +1,47 @@
 class IdeasController < ApplicationController
   def index
-    # @category = inputs[:category]
-    # if @category.nil?
-    #   @idea = Idea.all
-    # else
-    #   @idea = Idea.find(@category)
-    # end
+    category_name = params[:category_name]
+    # nilの場合
+    if category_name.nil?
+      @ideas = Idea.all
+    else
+      category = Category.find_by(category_name:category_name)
+      if category.nil?
+        render status: 404
+      else
+        @ideas = Idea.where(category_id:category.id)
+      end
+    end
+    render json: @ideas
   end
+  #パラメータで遅れているかどうか
+  #送れていない場合は全権取得できているか
+  #カテゴリーがある場合かつ、カテゴリーが見つかるか見つからないかのパターンでテストする
+  #見つかった場合は、正しいデータが返せているか、見つからない場合は404が返せているか
 
   def new
     @idea = Idea.new
   end
 
   def create
+    # categoriesテーブルにnameが存在するか
+    category = Category.find_or_initialize_by(name:params[:category_name])
+    if category.new_record?
+      category.save
+    end
+
+    # categoriesテーブルにnameが存在する場合は、ideaテーブルに登録
     @idea = Idea.new(idea_params)
-    if @item.valid?
-      @item.save
-      render status: 201, json: { status: 201, message: "Success" }
+    @idea.category_id = category.id
+    if @item.save
+      render status: 201, json: { message: "Success" }
     else
-      render status: 422, json: { status: 422, message: "Error"}
+      render status: 422, json: { message: "Error"}
     end
   end
 
-
   private 
   def idea_params
-    params.require(:idea).permit(:category, :body)
+    params.require(:idea).permit(:body)
   end
 end
